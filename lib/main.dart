22f3cb48555bd18096e65c11cdefcb659f01f65d
Daynial Khan <daynial.khan@alsonstechnology.com>
2021-12-21 18:08:33 +0500

    made login page add routes basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled/home_page.dart';
+import 'package:untitled/pages/home_page.dart';
+import 'package:untitled/pages/login_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -14,7 +15,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     double days = 30;
     return MaterialApp(
-      home:HomePage()
+   //   home:HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch:Colors.lightGreen),
+      darkTheme: ThemeData(
+       brightness: Brightness.light,
+        primarySwatch:Colors.deepPurple
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/" :(context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+
     );
   }
 }
