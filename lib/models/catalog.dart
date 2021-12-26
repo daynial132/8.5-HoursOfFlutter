dbbba524a8e935da922c884226c5a7d64477e7fa
Daynial Khan <daynial.khan@alsonstechnology.com>
2021-12-26 14:55:33 +0500

    Drawer is made adding products to catalog

---
+++ lib/models/catalog.dart
@@ -0,0 +1,27 @@
+class item {
+  final String Id;
+  final String Name;
+  final String Description;
+  final num Price;
+  final String Color;
+  final String image;
+
+  item(
+      {required this.Id,
+      required this.Name,
+      required this.Description,
+      required this.Price,
+      required this.Color,
+      required this.image});
+}
+
+final Product = [
+  item(
+      Id: "1",
+      Name: "Iphone1",
+      Description: "Apple Iphone 12Gen",
+      Price: 999,
+      Color: "black",
+      image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/IPhone_1st_Gen.svg/200px-IPhone_1st_Gen.svg.png")
+];
